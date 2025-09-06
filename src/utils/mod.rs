5b4a225fa6f3b7e38b79cf7161c222caf0cdb39a
Eidi Ueno <eidhie@gmail.com>
2025-09-06 15:50:46 +0900

    refactor: Add convenience re-exports to utils module

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,2 +1,6 @@
 pub mod colors;
 pub mod paths;
+
+// Re-export commonly used functions for convenience
+pub use colors::{colorize_environment, format_default_marker, mask_value};
+pub use paths::{find_project_root, get_config_path};
